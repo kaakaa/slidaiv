86cbb7a3a470f282c09929da603e0e8525ef03f6
kaakaa <stooner.hoe@gmail.com>
2024-06-23 22:15:18 +0900

    chore: remove unnecessary comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 import type {SlidevMarkdown, SourceSlideInfo} from '@slidev/types';
@@ -9,22 +7,11 @@ import {ExtensionID} from './constants';
 import {Client} from './client/openai';
 import { obj2frontmatter } from './utils';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "demo-webpack" is now active!');
-
 	const apiKey:string = vscode.workspace.getConfiguration(ExtensionID).get('apiKey') || '';
 	const baseUrl:string|null = vscode.workspace.getConfiguration(ExtensionID).get('baseUrl') || null;
-
 	const client = new Client(apiKey, baseUrl);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('demo-webpack.helloWorld', async () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
