8a93fcd002864d1a7072c53d2d53e67f09183c0b
Yusuke Nemoto <kaakaa@users.noreply.github.com>
2024-07-03 22:45:19 +0900

    feat: handle error correctly and show it to user (#9)

---
+++ src/model/slidev.ts
@@ -0,0 +1,45 @@
+import { parse } from "@slidev/parser";
+import { SourceSlideInfo } from "@slidev/types";
+import { obj2frontmatter } from "../utils";
+
+export class SlidevPage {
+    private _start: number
+    private _end: number
+    private frontmatter: any
+    private prompts: string[]
+
+    // constructor is private and the instance is created by factory method `init`,
+    // because it isn't possible to use await in constructor
+    private constructor(slide: SourceSlideInfo) {
+        this._start = slide.start;
+        this._end = slide.end;
+        this.frontmatter = slide.frontmatter;
+        this.prompts = slide.frontmatter?.slidaiv?.prompt || [];
+        if (this.prompts.length === 0) {
+            throw new Error('No prompt found in the slide frontmatter');
+        }
+    }
+
+    static async init(text: string, filename: string, pos: number) {
+        const parsed = await parse(text, filename);
+        const slide = parsed.slides.find((slide: SourceSlideInfo) => slide.start <= pos && pos <= slide.end );
+        if (!slide) {
+            throw new Error(`No slide found at line:${pos}`);
+        }
+        return new SlidevPage(slide);
+    }
+
+    async rewriteByLLM(client: LLMClient, model: string) {
+        const prompt = this.prompts.map((prompt: string) => `- ${prompt}`).join('\n');
+        const content = await client.generatePageContents(prompt, model);
+        return `${obj2frontmatter(this.frontmatter)}\n\n${content}\n\n`;
+    }
+
+    get start(): number {
+        return this._start;
+    }
+
+    get end(): number {
+        return this._end;
+    }
+}
