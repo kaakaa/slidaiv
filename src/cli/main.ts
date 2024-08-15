1d26dc5cc641e65df51c4dda3d5f4c09ab1bdc2f
kaakaa <stooner.hoe@gmail.com>
2024-08-15 23:00:34 +0900

    refactoring: move some values/funcs to util.ts

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,36 +1,12 @@
 import { Command } from 'commander';
 import fs from 'fs';
-import yaml from 'yaml';
 
 import { MultiBar, Presets } from 'cli-progress';
 import { OpenAI } from 'openai';
 
 import { parse } from "@slidev/parser";
-import { getDefaultPromptDecorateContents, getDefaultPromptForGenerateContents } from '@/client/prompts';
-import { Configuration } from '@/model/config';
 
-// TODO: logging
-// TODO: refactoring
-
-const SlidevHeader = `---
-theme: seriph
-highlighter: shiki
-background: https://source.unsplash.com/collection/94734566/1920x1080
-class: text-center
-info: |
-  ## 2024mmdd_web-diff
-  Presentation slides for 2024mmdd_web-diff.
-transition: slide-left
-title: 2024mmdd_web-diff
-mdc: true
-githubPages:
-  ogp: true
-addons:
-  - slidev-addon-rabbit
-rabbit:
-  slideNum: true
----
-`;
+import { GeneratedSlide, loadConfig, SlidevHeader } from '@/cli/util';
 
 const program = new Command();
 program
@@ -42,65 +18,22 @@ program
   .option('-m, --model <model>', 'model of openai api')
   .option('-d, --debug', 'output extra debugging', false);
 
-program.parse();
-
-const options = program.opts();
-const {input, output, locale, apiurl, apikey, model, debug} = options;
-
-
-type Merge<T> = { [K in keyof T]: T[K] };
-type Slide = {
-  title: string;
-  prompts: string[];
-}
-type CLIConfiguration = {
-  input: string;
-  output: string;
-  locale: string;
-}
-type config = {
-  service: Merge<Configuration & CLIConfiguration>;
-  slides: Slide[];
-}
-type GeneratedSlide = {
-  index: number;
-  contents: string;
-}
-
-const f = fs.readFileSync(input, 'utf8');
-const conf = yaml.parse(f) as config;
-
-const config = {
-  apiKey: apikey ?? conf.service.apiKey,
-  baseUrl: apiurl ?? conf.service.baseUrl,
-  model: model ?? conf.service.model,
-  promptGenerate: getDefaultPromptForGenerateContents(locale),
-  promptDecorate: getDefaultPromptDecorateContents(),
-  isDebug: debug ?? true,
-};
-
-console.log(`  input: ${input}`);
-console.log(`  output: ${output}`);
-console.log(`  locale: ${locale}`);
-console.log(`  apiurl: ${apiurl}`);
-console.log(`  apikey: xxxx`);
-console.log(`  model: ${model}`);
-console.log(`  debug: ${debug}`);
+const options = program.parse().opts();
+const config = loadConfig(fs.readFileSync(options.input, 'utf8'), options);
 console.log(config);
 
-const client = new OpenAI({ apiKey: config.apiKey, baseURL: config.baseUrl });
+const client = new OpenAI({ apiKey: config.service.apiKey, baseURL: config.service.baseUrl });
 const pages: GeneratedSlide[] = [];
-// const prompts = ["What your name?", "Where are you from?", "How old are you?"];
 
 const multi = new MultiBar({}, Presets.shades_classic);
-const progress = multi.create(conf.slides?.length, 0);
+const progress = multi.create(config.slides?.length, 0);
 multi.log("Generating slides...\n");
 
-const promises = conf.slides.map((slide, idx) => {
+const promises = config.slides.map((slide, idx) => {
   return client.chat.completions.create({
-    model: config.model,
+    model: config.service.model,
     messages: [
-      {"role": "system", "content": config.promptGenerate},
+      {"role": "system", "content": config.service.promptGenerate},
       {"role": "user", "content": slide.prompts.join("\n")},
     ],
   }).then((resp) => {
@@ -115,8 +48,8 @@ Promise.all(promises).then(() => {
   progress.stop();
   multi.stop();
   pages.sort((a, b) => a.index - b.index);
-  parse(pages.map((p) => p.contents).join("\n"), output).then((parsed) => {
-    fs.writeFileSync(output, `${SlidevHeader}\n${parsed.raw}\n`);
+  parse(pages.map((p) => p.contents).join("\n"), config.service.output).then((parsed) => {
+    fs.writeFileSync(config.service.output, `${SlidevHeader}\n${parsed.raw}\n`);
     console.log("Done");
   });
 });
---
+++ src/cli/util.ts
@@ -0,0 +1,74 @@
+import yaml from 'yaml';
+
+import { Configuration } from '@/model/config';
+import type { OptionValues } from 'commander';
+
+import {
+    getDefaultPromptDecorateContents,
+    getDefaultPromptForGenerateContents
+} from '@/client/prompts';
+
+export type Merge<T> = { [K in keyof T]: T[K] };
+
+export type Slide = {
+    title: string;
+    prompts: string[];
+}
+
+export type CLIOptions = {
+    input: string;
+    output: string;
+    locale: string;
+}
+
+export type CLIConfiguration = {
+    service: Merge<Configuration & CLIOptions>;
+    slides: Slide[];
+}
+
+export type GeneratedSlide = {
+    index: number;
+    contents: string;
+}
+
+export const SlidevHeader = `---
+theme: seriph
+highlighter: shiki
+background: https://source.unsplash.com/collection/94734566/1920x1080
+class: text-center
+info: |
+  ## 2024mmdd_web-diff
+  Presentation slides for 2024mmdd_web-diff.
+transition: slide-left
+title: 2024mmdd_web-diff
+mdc: true
+githubPages:
+  ogp: true
+addons:
+  - slidev-addon-rabbit
+rabbit:
+  slideNum: true
+---
+`;
+
+export function loadConfig(f: string, options: OptionValues): CLIConfiguration {
+    const { input, output, locale, apiurl, apikey, model, debug } = options;
+    const conf = yaml.parse(f) as CLIConfiguration;
+
+    const loc = locale ?? conf.service.locale;
+
+    return {
+        service: {
+            apiKey: apikey ?? conf.service.apiKey,
+            baseUrl: apiurl ?? conf.service.baseUrl,
+            model: model ?? conf.service.model,
+            promptGenerate: getDefaultPromptForGenerateContents(loc),
+            promptDecorate: getDefaultPromptDecorateContents(),
+            input: input ?? conf.service.input,
+            output: output ?? conf.service.output,
+            locale: loc,
+            isDebug: debug ?? true,
+        },
+        slides: conf.slides,
+    };
+}
